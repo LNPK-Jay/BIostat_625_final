941854fd494531b40e0bbf2018c8469a08842158
Jiawei Ji <jasonjjw@umich.edu>
2024-12-17 17:42:48 -0500

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,6 +1,6 @@
 import torch
 import segmentation_models_pytorch as smp
-
+#Using pretrained model
 def get_model(device):
     model = smp.Unet(encoder_name="resnet34", encoder_weights="imagenet", in_channels=3, classes=1)
     return model.to(device)
